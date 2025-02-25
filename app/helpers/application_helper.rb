cb6381880dc5c469f74cd2331154e75f0561833e
Amina <69321218+amina-crypto@users.noreply.github.com>
2025-02-25 22:52:26 +0100

    Added styling for each view of the pages of the annonymous user , library personel and library user . Addes styling for the pages profile.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type.to_sym
+    when :notice, :success
+      'is-success'
+    when :alert, :error
+      'is-danger'
+    when :warning
+      'is-warning'
+    else
+      'is-info'
+    end
+  end
+
+  def flash_icon(type)
+    case type.to_sym
+    when :notice, :success
+      'fa-check-circle'
+    when :alert, :error
+      'fa-exclamation-circle'
+    when :warning
+      'fa-exclamation-triangle'
+    else
+      'fa-info-circle'
+    end
+  end
 end
